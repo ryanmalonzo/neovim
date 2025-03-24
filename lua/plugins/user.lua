a7acf6110c637d702abe9fe945958dce9aa8a383
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-03-24 16:28:51 +0100

    Install catppuccin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,21 @@
 ---@type LazySpec
 return {
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup {
+        flavour = "mocha",
+        transparent_background = true,
+        integrations = {
+          blink_cmp = true,
+        },
+      }
+    end,
+  },
+
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
@@ -9,21 +25,22 @@ return {
   -- Disable dashboard
   { "goolord/alpha-nvim", enabled = false },
 
+  -- Lazygit
   {
     "kdheepak/lazygit.nvim",
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
-  }
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 }
