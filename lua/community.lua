22918284a0a8e0dcb96ee98f31e1d6fcd82c4dd5
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-03-24 16:33:34 +0100

    Install blink-cmp

--- lua/community.lua
+++ lua/community.lua
@@ -3,4 +3,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.recipes.disable-tabline" },
+  { import = "astrocommunity.completion.blink-cmp" },
 }
