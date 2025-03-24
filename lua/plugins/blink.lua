f5874b46f42b710f81aa80287dae632ae0c83122
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-03-24 17:03:50 +0100

    Override blink.cmp configuration

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    keymap = {
+      preset = "default",
+    },
+  },
+}
