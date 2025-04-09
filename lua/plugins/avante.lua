f296a03d066511bcaa11503c8d61ea7fe7948a3e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-09 23:19:55 +0200

    Use Claude 3.7 Sonnet with OpenRouter

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,11 +5,11 @@ return {
     provider = "openrouter",
     vendors = {
       openrouter = {
-        __inherited_from = 'openai',
-        endpoint = 'https://openrouter.ai/api/v1',
-        api_key_name = 'OPENROUTER_API_KEY',
-        model = 'deepseek/deepseek-r1',
+        __inherited_from = "openai",
+        endpoint = "https://openrouter.ai/api/v1",
+        api_key_name = "OPENROUTER_API_KEY",
+        model = "anthropic/claude-3.7-sonnet",
       },
     },
-  }
+  },
 }
