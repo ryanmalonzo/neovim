fe0867773f201273342b66f39be13c502d47e90b
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-08 22:23:17 +0200

    feat(options): set common option values

---
+++ lua/config/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+
+opt.mouse = "a"
+
+opt.clipboard = "unnamedplus"
+
+
