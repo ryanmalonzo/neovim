7cb225665b669a4178f9a14f0c9bed5f8ea1cea1
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-08 22:56:28 +0200

    feat(snacks.nvim): configure picker + update lazygit keymap

--- lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap.set
-
-keymap("n", "<leader>lg", function()
-  Snacks.lazygit.open()
-end, { desc = "Open LazyGit" })
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,5 +5,11 @@ return {
   ---@type snacks.Config
   opts = {
     lazygit = {},
+    picker = {},
   },
+  keys = {
+    { "<leader>lg", function() Snacks.lazygit.open() end, desc = "Open LazyGit" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+  }
 }
