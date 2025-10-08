cf5bfaafe90afacad1bbf58fe50be902c29b56a1
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-08 23:26:47 +0200

    feat(plugins): install nvim-treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "vim", "vimdoc", "markdown", "javascript", "typescript" },
+      sync_install = false,
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
+}
