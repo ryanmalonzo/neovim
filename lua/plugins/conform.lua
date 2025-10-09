3b851de59147a0c4c810bed43944d793ac01a0d9
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-09 22:09:39 +0200

    feat(conform.nvim): format with biome

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,10 +5,10 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
-      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+      typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = { timeout_ms = 500 },
   },
