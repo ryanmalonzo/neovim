16074a9d95325c4bd18a21b8c589771deb6076eb
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-12 13:01:35 +0200

    chore(lint): ignore errors

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
-    require("lint").try_lint()
+    require("lint").try_lint(nil, {
+      ignore_errors = true,
+    })
   end,
 })
 
