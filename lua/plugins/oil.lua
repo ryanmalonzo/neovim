dda8c6aa1a7d4b0117ee9ebd0eb896103ed2a51e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-12 11:33:03 +0200

    feat(oil.nvim): autosave lsp file updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,14 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false,
+  opts = {
+    lsp_file_methods = {
+      enabled = true,
+      timeout_ms = 1000,
+      autosave_changes = true,
+    },
+  },
 }
