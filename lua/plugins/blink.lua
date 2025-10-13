10afd07880ad5265047f3ef439b79577a01b15cd
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-13 18:53:37 +0200

    feat(plugins): add copilot support

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,19 +1,44 @@
 return {
-  'saghen/blink.cmp',
-  version = '1.*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  cond = not vim.g.vscode,
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono'
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    cond = not vim.g.vscode,
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
     },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
   },
-  opts_extend = { "sources.default" }
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    cond = not vim.g.vscode,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = { documentation = { auto_show = false } },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
 }
