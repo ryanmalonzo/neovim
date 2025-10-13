5832bd2cb1380f521e47c92d1b2309b17d680808
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-10-13 18:56:13 +0200

    feat(lsp): add vim to globals

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,23 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  cond = not vim.g.vscode,
-  opts = {
-    ensure_installed = { "lua_ls", "vtsls" },
-  },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
     "neovim/nvim-lspconfig",
   },
+  cond = not vim.g.vscode,
+  config = function(_, opts)
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "vtsls" },
+    })
+
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+  end,
 }
