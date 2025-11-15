9d5f797130f744b3ed8317e36c75c810f27c1b7a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-11-15 23:48:22 +0100

    use winbar with status text

--- lua/community.lua
+++ lua/community.lua
@@ -4,4 +4,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.recipes.disable-tabline" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 }
