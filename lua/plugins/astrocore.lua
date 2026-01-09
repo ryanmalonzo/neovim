08f005f8d6624e6c2835a6c18b7d5ec6f0542221
Ryan Malonzo <ryan.malonzo@spendesk.com>
2026-01-09 10:35:19 +0100

    add copy buffer and paste clipboard keybinds

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -48,6 +48,28 @@ return {
           desc = "Copy relative path",
         },
 
+        -- copy whole buffer to clipboard
+        ["<C-c>"] = {
+          function()
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            local content = table.concat(lines, "\n")
+            vim.fn.setreg("+", content)
+            vim.notify("Buffer copied to clipboard", vim.log.levels.INFO)
+          end,
+          desc = "Copy buffer to clipboard",
+        },
+
+        -- paste from clipboard, replacing whole buffer
+        ["<C-v>"] = {
+          function()
+            local content = vim.fn.getreg "+"
+            local lines = vim.split(content, "\n")
+            vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+            vim.notify("Buffer replaced from clipboard", vim.log.levels.INFO)
+          end,
+          desc = "Paste from clipboard (replace buffer)",
+        },
+
         -- lazygit
         ["<Leader>tl"] = false,
         ["<Leader>lg"] = {
