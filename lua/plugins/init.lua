1f0c2ce6c6e39244b830721552701e51d45439ed
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-16 20:58:37 +0100

    install guess-indent.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,78 +1,86 @@
 vim.pack.add({ "https://github.com/zuqini/zpack.nvim" })
 
 require("zpack").setup({
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({ flavour = "mocha" })
-            vim.cmd.colorscheme("catppuccin")
-        end,
-    },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({ flavour = "mocha" })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter").setup({})
-            require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "markdown", "markdown_inline" })
-        end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({})
+      require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "markdown", "markdown_inline" })
+    end,
+  },
 
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("plugins.lsp")
-        end,
-    },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("plugins.lsp")
+    end,
+  },
 
-    {
-        "nvim-telescope/telescope.nvim",
-        cmd = "Telescope",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        keys = {
-            { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-            { "<leader>fw", "<cmd>Telescope live_grep<cr>" },
-            { "<leader>fb", "<cmd>Telescope buffers<cr>" },
-            { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
-        },
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fw", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
     },
+  },
 
-    {
-        "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("gitsigns").setup({
-                on_attach = function(bufnr)
-                    local gs = require("gitsigns")
-                    local opts = { buffer = bufnr }
-                    vim.keymap.set("n", "]c", gs.next_hunk, opts)
-                    vim.keymap.set("n", "[c", gs.prev_hunk, opts)
-                    vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
-                    vim.keymap.set("n", "<leader>hb", gs.blame_line, opts)
-                end,
-            })
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = require("gitsigns")
+          local opts = { buffer = bufnr }
+          vim.keymap.set("n", "]c", gs.next_hunk, opts)
+          vim.keymap.set("n", "[c", gs.prev_hunk, opts)
+          vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
+          vim.keymap.set("n", "<leader>hb", gs.blame_line, opts)
         end,
-    },
+      })
+    end,
+  },
 
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup()
-        end,
-    },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup()
+    end,
+  },
 
-    {
-        "numToStr/Comment.nvim",
-        keys = {
-            { "gc", mode = { "n", "v" } },
-            { "gcc", mode = "n" },
-        },
-        config = function()
-            require("Comment").setup()
-        end,
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "gc", mode = { "n", "v" } },
+      { "gcc", mode = "n" },
     },
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+
+  {
+    "NMAC427/guess-indent.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("guess-indent").setup()
+    end,
+  }
 })
