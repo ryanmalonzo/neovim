33b6ff42888ae79e08ba111eb673be77691776ba
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-16 21:36:16 +0100

    make background transparent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,7 +6,10 @@ require("zpack").setup({
     name = "catppuccin",
     priority = 1000,
     config = function()
-      require("catppuccin").setup({ flavour = "mocha" })
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
       vim.cmd.colorscheme("catppuccin")
     end,
   },
