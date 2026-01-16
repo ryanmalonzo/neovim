29eb214962642d184e1dc7da954b9dc01da7bfd9
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-16 20:27:59 +0100

    install catppuccin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,13 @@
 vim.pack.add({ "https://github.com/zuqini/zpack.nvim" })
+
+require("zpack").setup({
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({ flavour = "mocha" })
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+})
