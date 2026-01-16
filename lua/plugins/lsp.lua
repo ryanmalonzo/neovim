1016e27b5744487d0faea48efc6b392de6b33a6d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-17 00:09:29 +0100

    format lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,30 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local opts = { buffer = args.buf }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-    end,
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  end,
 })
 
 vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-        },
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
     },
+  },
 })
 
 vim.lsp.config("vtsls", {
-    settings = {
-        typescript = {
-            tsserver = {
-                maxTsServerMemory = 8192,
-            },
-        },
+  settings = {
+    typescript = {
+      tsserver = {
+        maxTsServerMemory = 8192,
+      },
     },
+  },
 })
