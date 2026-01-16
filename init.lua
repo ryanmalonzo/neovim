ad54e5a5bd9e4fb78214e554a6266d872ba8ab25
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-16 20:13:30 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+vim.opt.number = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.undofile = true
+vim.opt.swapfile = false
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.mouse = "a"
+vim.opt.showmode = false
