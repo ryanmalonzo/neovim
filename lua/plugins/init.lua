e0d05933c348ff380ec8c1efe559d8b4fc97f150
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-16 20:49:05 +0100

    install gitsigns

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,4 +39,21 @@ require("zpack").setup({
             { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
         },
     },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gs = require("gitsigns")
+                    local opts = { buffer = bufnr }
+                    vim.keymap.set("n", "]c", gs.next_hunk, opts)
+                    vim.keymap.set("n", "[c", gs.prev_hunk, opts)
+                    vim.keymap.set("n", "<leader>hp", gs.preview_hunk, opts)
+                    vim.keymap.set("n", "<leader>hb", gs.blame_line, opts)
+                end,
+            })
+        end,
+    },
 })
