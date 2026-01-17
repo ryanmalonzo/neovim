663124c54a3e85a738fdb05b65cbf4b83a2e0651
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-17 10:56:31 +0100

    add split navigation keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,9 @@
+-- Split navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+
 -- Copy relative path
 vim.keymap.set("n", "<Leader>fy", function()
   local path = vim.fn.expand("%:.")
