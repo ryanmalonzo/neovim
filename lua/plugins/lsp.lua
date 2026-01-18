1c27c87758e2bf6f34eeffca09b4b838523460ee
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-18 10:23:55 +0100

    update lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,8 +4,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "gra", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
   end,
