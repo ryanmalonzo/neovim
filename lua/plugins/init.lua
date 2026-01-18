4aad3f6e152de64eb04747e0191567030020962a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-01-18 15:20:53 +0100

    install tiny-inline-diagnostic.nvim
    preferred over built-in `virtual_lines`:
    https://github.com/rachartier/tiny-inline-diagnostic.nvim/blob/ecce93ff7db4461e942c03e0fcc64bd785df4057/README.md#comparison-with-neovims-built-in-virtual_lines

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -379,4 +379,18 @@ require("zpack").setup({
       })
     end,
   },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    version = "ecce93ff7db4461e942c03e0fcc64bd785df4057",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        preset = "simple",
+      })
+      -- Disable Neovim's default virtual text diagnostics
+      vim.diagnostic.config({ virtual_text = false })
+    end,
+  },
 })
