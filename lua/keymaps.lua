99d7d7afd22ea6352db3c063f9f1226a93bcbe3f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-02-24 23:31:17 +0100

    use smart-splits.nvim
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,3 +33,22 @@ vim.keymap.set("n", "<C-v>", function()
   vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
   vim.notify("Buffer replaced from clipboard", vim.log.levels.INFO)
 end, { desc = "Paste from clipboard (replace buffer)" })
+
+-- smart-splits.nvim
+
+-- resizing splits
+vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
+-- swapping buffers between windows
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -490,23 +490,8 @@ require("zpack").setup({
   },
 
   {
-    "christoomey/vim-tmux-navigator",
-    version = "v1.0",
+    "mrjones2014/smart-splits.nvim",
     cond = not vim.g.vscode,
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
-    },
+    lazy = false,
   },
 })
