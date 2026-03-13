d49ee101642e2bf96906b7dcdae398fc62106a2f
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-03-13 20:12:42 +0100

    install sidekick.nvim
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -487,4 +487,82 @@ require("zpack").setup({
       require("nvim-ts-autotag").setup()
     end,
   },
+
+  {
+    "folke/sidekick.nvim",
+    cond = not vim.g.vscode,
+    keys = {
+      {
+        "<leader>aa",
+        function()
+          require("sidekick.cli").toggle()
+        end,
+        desc = "Toggle AI CLI",
+      },
+      {
+        "<leader>ac",
+        function()
+          require("sidekick.cli").toggle({ name = "claude", focus = true })
+        end,
+        desc = "Toggle Claude Code",
+      },
+      {
+        "<leader>ao",
+        function()
+          require("sidekick.cli").toggle({ name = "opencode", focus = true })
+        end,
+        desc = "Toggle OpenCode",
+      },
+      {
+        "<leader>as",
+        function()
+          require("sidekick.cli").select({ filter = { installed = true } })
+        end,
+        desc = "Select AI CLI tool",
+      },
+      {
+        "<leader>at",
+        function()
+          require("sidekick.cli").send({ msg = "{this}" })
+        end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>ab",
+        function()
+          require("sidekick.cli").send({ msg = "{file}" })
+        end,
+        desc = "Send Buffer",
+      },
+      {
+        "<leader>av",
+        function()
+          require("sidekick.cli").send({ msg = "{selection}" })
+        end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function()
+          require("sidekick.cli").prompt()
+        end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+    },
+    config = function()
+      require("sidekick").setup({
+        nes = { enabled = false },
+        cli = {
+          watch = true,
+          win = {
+            layout = "right",
+            split = { width = 80 },
+          },
+        },
+      })
+    end,
+  },
 })
