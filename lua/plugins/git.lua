39a4111f34f0f2c3b37e1ea61fa1e4a9bb8e9888
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-04-08 23:41:25 +0200

    install diffview.nvim
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -19,6 +19,31 @@ return {
   },
 
   {
+    "dlyongemallo/diffview.nvim",
+    commit = "f81170ee78304944fc1dba1141d371a6811ee38a",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewToggle",
+      "DiffviewFileHistory",
+      "DiffviewDiffFiles",
+      "DiffviewLog",
+      "DiffviewClose",
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+      { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "Current file history" },
+      { "<leader>gf", ":DiffviewFileHistory<CR>", mode = "v", desc = "Selection history" },
+    },
+    cond = not vim.g.vscode,
+    config = function()
+      require("diffview").setup({
+        enhanced_diff_hl = true,
+        use_icons = true,
+      })
+    end,
+  },
+
+  {
     "Almo7aya/openingh.nvim",
     version = "v1.0.14",
     keys = {
