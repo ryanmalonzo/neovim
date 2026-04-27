994b70b665fc73a1ad2434909a71395ba698f2b5
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-04-28 00:04:38 +0200

    autoload correct theme
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    if vim.o.background == "light" then
+      vim.cmd.colorscheme("catppuccin-latte")
+    else
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
   callback = function()
     if vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 then
