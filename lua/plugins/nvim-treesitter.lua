5b81917448da3e9df071e587b2ac2236f95491af
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-01 19:45:22 +0200

    feat: install nvim-treesitter
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install {
+        "css",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "jsx",
+        "lua",
+        "markdown",
+        "nix",
+        "prisma",
+        "terraform",
+        "toml",
+        "tsx",
+        "typescript",
+        "yaml",
+      }
+    end,
+  }
+}
