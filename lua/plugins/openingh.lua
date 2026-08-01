f41a8bf9a612cfbfaae0999ea7caa31f09af7c89
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-01 22:11:46 +0200

    feat: install openingh.nvim
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

---
+++ lua/plugins/openingh.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "Almo7aya/openingh.nvim",
+    keys = {
+      { "<leader>gO", "<cmd>OpenInGHFile<cr>", desc = "Open file in GitHub" },
+      { "<leader>gO", ":OpenInGHFileLines<CR>", mode = "v", desc = "Open file with lines in GitHub" },
+    },
+  },
+}
