c0ddef17ddc326947a96f3f40f2270baf816e9d0
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-01 21:16:37 +0200

    feat: remove lualine separators
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
+      options = {
+        section_separators = "",
+        component_separators = "",
+      },
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diagnostics" },
