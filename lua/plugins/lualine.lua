a909914463200d11cd9bf91cf8ab561ac749081a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-01 21:02:16 +0200

    feat: configure lualine
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,23 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = {}
+    opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = { "location" },
+        lualine_z = { "lsp_status" },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
   },
 }
