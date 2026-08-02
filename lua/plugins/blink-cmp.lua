1228233b9c195ce52beb39677704ec1818e8062d
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-02 08:43:19 +0200

    feat: install lazydev.nvim
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -11,7 +11,14 @@ return {
       },
       completion = { documentation = { auto_show = false } },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
       },
       fuzzy = { implementation = "prefer_rust_with_warning" },
     },
