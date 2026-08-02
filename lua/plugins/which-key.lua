e54eb13e41abdebfebc05a0a8b52b7c2cf8d3816
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-02 09:16:11 +0200

    chore: remove which-key keymap
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,14 +3,5 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {},
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
   },
 }
