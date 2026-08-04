c1049e5c70b600d06ff7461347932b1c6646d09e
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-08-04 21:37:57 +0200

    feat: enable syntax highlighting for treesitter parsers
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,25 +1,43 @@
+local languages = {
+  "css",
+  "dockerfile",
+  "html",
+  "javascript",
+  "json",
+  "jsx",
+  "lua",
+  "markdown",
+  "nix",
+  "prisma",
+  "terraform",
+  "toml",
+  "tsx",
+  "typescript",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").install({
-        "css",
-        "dockerfile",
-        "html",
-        "javascript",
-        "json",
-        "jsx",
-        "lua",
-        "markdown",
-        "nix",
-        "prisma",
-        "terraform",
-        "toml",
-        "tsx",
-        "typescript",
-        "yaml",
+      require("nvim-treesitter").install(languages)
+
+      local language_to_filetype = {
+        jsx = "javascriptreact",
+        tsx = "typescriptreact",
+      }
+
+      local filetypes = vim.tbl_map(function(lang)
+        return language_to_filetype[lang] or lang
+      end, languages)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function()
+          vim.treesitter.start()
+        end,
       })
     end,
   },
